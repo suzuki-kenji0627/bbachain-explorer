42cd92f1c307e57e2a5c826d362007a730a2ff35
suzuki-kenji0627 <masaru.fa0207@gmail.com>
2023-05-06 06:39:28 +0300

    update loading component

--- src/components/common/LoadingCard.tsx
+++ src/components/common/LoadingCard.tsx
@@ -2,10 +2,19 @@ import React from "react";
 
 export function LoadingCard({ message }: { message?: string }) {
   return (
-    <div className="card">
+    <div className="card w-full bg-base-100 shadow-xl">
       <div className="card-body text-center">
-        <span className="spinner-grow spinner-grow-sm me-2"></span>
-        {message || "Loading"}
+        <h2 className="card-title">{message || "Loading"}</h2>
+        <div className="text-center">
+          <div
+            className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+            role="status">
+            <span
+              className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
+              >{message || "Loading..."}</span
+            >
+          </div>
+        </div>
       </div>
     </div>
   );
